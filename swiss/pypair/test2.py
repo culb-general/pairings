9b8235632c25c1b22828c4190c835cfe3cd6b2f2
culb-general <wildpanties@yandex.ru>
2015-11-15 03:21:14 +0300

    improve result printing in test script

--- swiss/pypair/test2.py
+++ swiss/pypair/test2.py
@@ -25,29 +25,23 @@ for i in range(players_cnt):
     to.addPlayer(str(i+1), "Player%s" % str(i+1), False)
 
 for i in range(cnt):
+    print("==========\nRound %s\n==========" % str(i + 1))
     pairings1 = to.pairRound()
-    print(pairings1)
+    #print(pairings1)
 
     for table in pairings1:
         if not type(pairings1[table]) is str:
             #print(pairings1[table])
             pts = random.randint(0,20)
             to.reportMatch(table, [pts, 20-pts, 0])
-            print("%s vs %s -- %s:%s" % (pairings1[table][0], pairings1[table][1], pts, 20-pts))
+            print("%s vs %s on table %s -- %s:%s" % (pairings1[table][0], pairings1[table][1], table, pts, 20-pts))
     to.saveEventData("%s/datadump%s.txt"%(home,i))
-    print("")
-#print to.playersDict[256]
-    print("")
-#print(json.dumps(to.dumpTournamentData()))
-
-#tournament_table = list(to.playersDict.items()).sort(key=operator.itemgetter('Points'))
-player_arr = []
-for key, val in six.iteritems(to.playersDict):
-    player_arr.append(val)
-#tournament_table = player_arr.sort(key=operator.itemgetter('Points'))
-
-for player in sorted(player_arr, key=operator.itemgetter('Points'), reverse=True):
-	print("%s: %s" % (player['Name'], player['Points']))
+    player_arr = []
+    for key, val in six.iteritems(to.playersDict):
+        player_arr.append(val)
+    for player in sorted(player_arr, key=operator.itemgetter('Points'), reverse=True):
+        print("%s: %s" % (player['Name'], player['Points']))
+    print('')
 
 #print("")
 #print(to.playersDict[1])
